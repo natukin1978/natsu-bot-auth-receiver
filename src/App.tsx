9327911ea2006fd2ec2c13b72ca0c0a8c66faac8
natukin1978 <natukin1978@hotmail.com>
2025-03-22 00:58:44 +0900

    Implemented to display the access token.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accessToken, setAccessToken] = useState<string>("");
+
+  const scope = [
+    "chat:read",
+    "chat:edit",
+    "moderator:manage:banned_users",
+  ].join(" ");
+  const params = {
+    client_id: "g1gb6b5as9zb48wlde0v0ux3frg2i3",
+    redirect_uri: "https://natukin1978.github.io/natsu-bot-auth-receiver/",
+    response_type: "token",
+    scope: scope,
+  }
+  const params_str = new URLSearchParams(params).toString();
+  const url_twitch_auth = "https://id.twitch.tv/oauth2/authorize?" + params_str;
+
+  const text_twitch_auth = "Twitch認証";
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const accessTokenMatch = hash.match(/access_token=([^&]*)/);
+      if (accessTokenMatch && accessTokenMatch[1]) {
+        setAccessToken(accessTokenMatch[1]);
+      }
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>
+        ナツBOT OAuth認証
+      </h2>
+      <h3>
+        作業手順
+      </h3>
+      <ol>
+        <li>下の[{text_twitch_auth}]を押してください</li>
+        <li>Twitchのログインページが表示されたら、BOTとして振る舞いたいユーザーでログインしてください</li>
+        <li>ログインページが表示されず、違うユーザーが表示されたら、一度Twitchのページに行きログアウトしてください</li>
+        <li>表示ユーザーで問題なければ、認証(Authorize)を押してください</li>
+        <li>正常に処理が進むと、このページに戻って来るので access_token の値をコピーします</li>
+        <li>アプリの config.json を開いて、twitch.accessToken に設定して保存してください</li>
+      </ol>
+      Twitch配信チャットBOT(twitch-chat-bot)向け <a href={url_twitch_auth}>{text_twitch_auth}</a>
+      {accessToken && (
+        <div>
+          <label>
+            access_token
+          </label>
+          &nbsp;:&nbsp;
+          <input type="text" value={accessToken} size={30} readOnly />
+        </div>
+      )}
     </>
   )
 }
