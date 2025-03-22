8fc9426225a43756a8b380f35bf246d9a1b72103
natukin1978 <natukin1978@hotmail.com>
2025-03-22 16:16:43 +0900

    Changed to make it easier to list multiple bots.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  const [accessToken, setAccessToken] = useState<string>("");
+const text_twitch_auth = "Twitch認証";
 
-  const scope = [
-    "chat:read",
-    "chat:edit",
-    "moderator:manage:banned_users",
-  ].join(" ");
+const get_url_twitch_auth = (scopes: string[]): string => {
+  const scope = scopes.join(" ");
   const params = {
     client_id: "g1gb6b5as9zb48wlde0v0ux3frg2i3",
     redirect_uri: "https://natukin1978.github.io/natsu-bot-auth-receiver/",
     response_type: "token",
     scope: scope,
   }
+  const end_point = "https://id.twitch.tv/oauth2/authorize";
   const params_str = new URLSearchParams(params).toString();
-  const url_twitch_auth = "https://id.twitch.tv/oauth2/authorize?" + params_str;
+  return end_point + "?" + params_str;
+}
 
-  const text_twitch_auth = "Twitch認証";
+const auth_infos = [
+  {
+    title: "Twitch配信チャットBOT",
+    application_link: "https://github.com/natukin1978/twitch-chat-bot",
+    scopes: [
+      "chat:read",
+      "chat:edit",
+      "moderator:manage:banned_users",
+    ],
+  },
+];
+
+function App() {
+  const [accessToken, setAccessToken] = useState<string>("");
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -34,20 +46,65 @@ function App() {
       <h2 style={{ borderBottom: "1px solid #000" }}>
         ナツキソBOT OAuth認証
       </h2>
-      <h3>
-        作業手順
-      </h3>
-      <ol>
-        <li>下の[{text_twitch_auth}]を押してください</li>
-        <li>Twitchのログインページが表示されたら、BOTとして振る舞いたいユーザーでログインしてください</li>
-        <li>ログインページが表示されず、違うユーザーが表示されたら、一度Twitchのページに行きログアウトしてください</li>
-        <li>表示ユーザーで問題なければ、認証(Authorize)を押してください</li>
-        <li>正常に処理が進むと、このページに戻って来るので access_token の値をコピーします</li>
-        <li>アプリの config.json を開いて、twitch.accessToken に設定して保存してください</li>
-      </ol>
-      Twitch配信チャットBOT(twitch-chat-bot)向け <a href={url_twitch_auth}>{text_twitch_auth}</a>
+      <div>
+        <h3>
+          作業手順
+        </h3>
+        <ol>
+          <li>下の[{text_twitch_auth}]ボタンを押してください</li>
+          <li>Twitchのログインページが表示されたら、BOTにしたいユーザーでログインしてください</li>
+          <li>ログインページが表示されず、違うユーザーが表示されたら、一度Twitchのページでログアウトしてください</li>
+          <li>表示ユーザーで問題なければ、認証(Authorize)を押してください</li>
+          <li>正常に処理が進むと、このページに戻って来るので access_token の値をコピーします</li>
+          <li>アプリの config.json を開いて、twitch.accessToken に設定して保存してください</li>
+        </ol>
+      </div>
+      <div>
+        <h3>
+          一覧
+        </h3>
+        <table>
+          <thead>
+            <tr>
+              <th>用途</th>
+              <th>認証</th>
+              <th>使用スコープ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              auth_infos.map(auth_info => (
+                <tr>
+                  <td>
+                    <a href={auth_info.application_link} target="_blank" rel="noopener noreferrer">
+                      {auth_info.title}
+                    </a>
+                  </td>
+                  <td>
+                    <a href={get_url_twitch_auth(auth_info.scopes)} className="a_to_button">
+                      {text_twitch_auth}
+                    </a>
+                  </td>
+                  <td>
+                    <ul>
+                      {
+                        auth_info.scopes.map(scope => (
+                          <li>{scope}</li>
+                        ))
+                      }
+                    </ul>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
       {accessToken && (
         <div>
+          <h3>
+            結果
+          </h3>
           <label>
             access_token
           </label>
@@ -55,6 +112,15 @@ function App() {
           <input type="text" value={accessToken} size={30} readOnly />
         </div>
       )}
+      <div>
+        <h3>
+          その他
+        </h3>
+        このページのソースコードはこちらで公開してます。
+        <a href="https://github.com/natukin1978/natsu-bot-auth-receiver" target="_blank" rel="noopener noreferrer">
+          Github
+        </a>
+      </div>
     </>
   )
 }
